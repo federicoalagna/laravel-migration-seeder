c0ab9ceceb5c14fabea6d6913e6d4ad44a503d90
federicoalagna <federicoalagna01@gmail.com>
2024-07-09 15:45:10 +0200

    update template

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,16 +3,9 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
-use App\Models\Movie;
+
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index()
-    {
-
-        $movies = Movie::all();
-
-        return view('movies.index', compact('movies'));
-    }
 }
